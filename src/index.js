133589a3cf0f413ba7e2dab21207a83ffe4e4587
Sotatek-HongBui <149551610+Sotatek-HongBui@users.noreply.github.com>
2025-03-17 23:00:24 +0100

    mvc

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ const morgan = require('morgan');
 const { engine } = require('express-handlebars'); // Use `engine` from express-handlebars
 const app = express();
 const port = 3000;
-
+const route = require('./routes')
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+    extended: true,
+}))
+app.use(express.json())
 
 // HTTP logger
 app.use(morgan('combined'));
@@ -17,13 +21,8 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home'); // Correct method to render a template
-});
 
-app.get('/news', (req, res) => {
-    res.render('news'); // Correct method to render a template
-});
+route(app)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
