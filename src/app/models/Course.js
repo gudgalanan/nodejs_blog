e7f125b281f5b44a8fcb41a521d2182d40d4b40f
Sotatek-HongBui <149551610+Sotatek-HongBui@users.noreply.github.com>
2025-03-20 06:32:11 +0100

    Auto increment id field

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 const slug = require('mongoose-slug-updater');
-var mongooseDelete = require('mongoose-delete');
-
+const mongooseDelete = require('mongoose-delete');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const Schema = mongoose.Schema;
 const Course = new Schema({
+    _id: { type: Number },
     name: { type: String, required: true },
     description: { type: String, required: true },
     imgage: { type: String, maxLength: 255 },
     slug: { type: String, slug: "name", unique: true },
 }, {
+    _id: false,
     timestamps: true,
 });
 
@@ -29,6 +31,7 @@ Course.query.sortable = function (req) {
 
 //Add plugin
 mongoose.plugin(slug);
+Course.plugin(AutoIncrement)
 Course.plugin(mongooseDelete, { deletedAt: true, overrideMethods: 'all' });
 
 module.exports = mongoose.model('Course', Course);
